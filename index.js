504ed01b65f348e42b7d650c00541ff24a141bec
emircan1x <emirkartallarx@gmail.com>
2025-11-27 01:48:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+const TOKEN = "MTM2MDk2ODU5NzMyNDk1OTg0NA.GYSUJM.yVuZ9rSnbI9arXOs5IQHtHw4Za-wt84YOUjPdI"; // Bot tokenini buraya ekle
+
+client.on("ready", () => {
+    console.log(`Bot giriş yaptı: ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+
+    if (message.content.toLowerCase() === "!sa") {
+        message.reply("as");
+    }
+});
+
+client.login(TOKEN);
